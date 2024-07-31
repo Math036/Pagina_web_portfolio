02686c4a4b07f5ad5994bd49e1606029b8183e38
Matheus Henrique Mota <92958911+Math036@users.noreply.github.com>
2024-07-31 16:28:08 -0300

    Update comissionScript.js

--- script/comissionScript.js
+++ script/comissionScript.js
@@ -6,7 +6,7 @@ function enableDisableTextBox(openCharacters){
     }
 }
 
-let totalPrice = 0;
+let totalPrice = 0.00;
 let addPrice;
 
 function catchNumber(number){
@@ -19,10 +19,38 @@ function catchNumber(number){
     }
 }
 
+function catchCharacterNunber(){
+    let charNumber = document.getElementById("characterNumber").value;
+    if(charNumber != null){
+        totalPrice = totalPrice + (charNumber * 10);
+    }
+}
+
 var priceMessage = document.getElementById("messageText");
 
 function showPrice(){
 
-    priceMessage.innerText = "O valor estimado da comissão é " + totalPrice + "reais.";
+    priceMessage.innerText = "O valor estimado da comissão é " + totalPrice + " reais.";
 
+}
+
+//Conversor de Moedas//
+
+async function convert(from, to) {
+    const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+    fetch('https://api.fastforex.io/convert?from=' + from + '&to=' + to + '&amount=' + totalPrice +'&api_key=47b95b05a1-fc6cc38e28-shi01d', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+
+    showConversion();
+      
+    return totalPrice;
+}
+
+var conversionText = document.getElementById("convertMessage");
+
+function showConversion() {
+    conversionText.innerText = "The Comission will cost " + totalPrice;
 }
