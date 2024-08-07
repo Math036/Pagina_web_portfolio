2b2f8db24a1b0d9e032e97027055874ba17970ae
Matheus Henrique Mota <92958911+Math036@users.noreply.github.com>
2024-08-07 16:45:16 -0300

    Update comissionScript.js

--- script/comissionScript.js
+++ script/comissionScript.js
@@ -10,16 +10,27 @@ let totalPrice = 0.00;
 let addPrice;
 
 function catchNumber(number){
-   
-    if(totalPrice == 0){
+    //verificar se opção já foi clicada
+   var clickCheck = document.getElementById("price");
+   addPrice = number;
+
+   if(clickCheck.checked){
+   }else if(totalPrice == 0){
         totalPrice = number;
-    }else{
-        addPrice = number;
+        }else{ 
         totalPrice = totalPrice + addPrice;
+        }
+}
+
+function subNumber(number){
+    var clickCheck = document.getElementById("price");
+
+    if (clickCheck.checked == false){
+        totalPrice = totalPrice - number;
     }
 }
 
-function catchCharacterNunber(){
+function catchCharacterNumber(){
     let charNumber = document.getElementById("characterNumber").value;
     if(charNumber != null){
         totalPrice = totalPrice + (charNumber * 10);
@@ -39,18 +50,23 @@ function showPrice(){
 async function convert(from, to) {
     const options = {method: 'GET', headers: {accept: 'application/json'}};
 
-    fetch('https://api.fastforex.io/convert?from=' + from + '&to=' + to + '&amount=' + totalPrice +'&api_key=47b95b05a1-fc6cc38e28-shi01d', options)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err));
+    const response = await fetch('https://api.fastforex.io/convert?from=' + from + '&to=' + to + '&amount=' + totalPrice +'&api_key=47b95b05a1-fc6cc38e28-shi01d', options);
+    const data = await response.json();
 
-    showConversion();
+    const conversionText = document.getElementById('convertMessage');
+    conversionText.innerText = data.results;
+
+    /*fetch('https://api.fastforex.io/convert?from=' + from + '&to=' + to + '&amount=' + totalPrice +'&api_key=47b95b05a1-fc6cc38e28-shi01d', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .then(response => {
+        const data = response;
+        let msg = ${};
+        const conversionText = document.getElementById("convertMessage");
+        conversionText.innerText = msg;
+      })
+      .catch(err => console.error(err));*/
       
-    return totalPrice;
+    return data.results;
 }
 
-var conversionText = document.getElementById("convertMessage");
-
-function showConversion() {
-    conversionText.innerText = "The Comission will cost " + totalPrice;
-}
